bf1c655d125cdc4d891f8fd5ee91d99aa274e936
MurilloPS <murillo18santos@gmail.com>
2024-08-03 10:10:21 -0300

    Sabado pokas ideias

---
+++ front/login.js
@@ -0,0 +1,33 @@
+let button = document.getElementById("handleSubmit");
+
+button.onclick = async function () {
+  
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+  
+    let data = {  email, password };
+
+    console.log("Dados do formulário:", data); 
+
+    const response = await fetch('http://localhost:3001/api/login', {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=UTF-8" },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const content = await response.json();
+        console.log("Resposta do servidor:", content); 
+        if (content.success) {
+            alert("Sucesso");
+        } else {
+            alert("Erro ao enviar os dados.");
+        }
+    } catch (error) {
+        console.error("Erro ao processar a resposta do servidor:", error);
+        alert("Erro ao processar a resposta do servidor. Por favor, tente novamente.");
+    }
+};
+function landing() {
+    window.location.href = "landing.html";
+}
